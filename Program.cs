7f4677b625c293fd298d43db87dc1f48775cd9fe
s31442 <s31442@pjwstk.edu.pl>
2025-05-09 14:06:45 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using Tutorial8.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<ITripsService, TripsService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
